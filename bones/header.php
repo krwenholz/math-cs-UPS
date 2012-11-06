77d7c45d8d16bc899e80a61252a84a313f8cdd39
Scott Dunham <dunham.scott@gmail.com>
2012-11-05 19:40:37 -0800

    Tables are now responsive

--- bones/header.php
+++ bones/header.php
@@ -30,12 +30,8 @@
 		<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,700,600' rel='stylesheet' type='text/css'>
         <!-- <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/css/style.css" /> -->
 		
-		<!-- Responsive Tables -->
-		<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/css/responsive-tables.css" />
-		<script type='text/javascript' src="<?php echo get_template_directory_uri(); ?>/js/responsive-tables.js"></script>
-		
 		<!-- Scripts -->
-		<script type='text/javascript' src='http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js?ver=1.3.2'></script>
+		<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
 		<?php if (is_home()): ?>
 		<script type='text/javascript' src="<?php echo get_template_directory_uri(); ?>/js/jcarousellite_1.0.1.min.js"></script>
 		<script>
@@ -60,6 +56,10 @@
 			});
 		</script>
 		<?php endif; ?>
+		
+		<!-- Zurb Responsive Tables -->
+		<link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/css/responsive-tables.css" />
+		<script type='text/javascript' src="<?php echo get_template_directory_uri(); ?>/js/responsive-tables.js"></script>
 			
 		<!-- drop Google Analytics Here -->
 		<!-- end analytics -->
